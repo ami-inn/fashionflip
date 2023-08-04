f7f0bcc2d2bc045a64bab8730c8fa92b8db9b815
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-08-04 17:13:39 +0530

    home jsx updated created custom button indexjs components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,70 @@
-import {motion,AnimatePresence} from 'framer-motion' //for some animation
-import  {useSnapshot} from 'valtio'
+import { motion, AnimatePresence } from "framer-motion"; //for some animation
+import { useSnapshot } from "valtio";
 
-import {headContainerAnimation, headContentAnimation,headTextAnimation,slideAnimation} from '../config/motion'
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
+import state from "../store";
+
+import { CustomButton } from "../components";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  const snap = useSnapshot(state); //so we can use the store values
 
-export default Home
+  return (
+    <AnimatePresence>
+      {/* component by frame motion thats going allows us to enable the animation of component that have been removed from the tree */}
+
+      {snap.intro && (
+        // if we are in intreo wee can render home
+        <motion.section className="home" {...slideAnimation("left")}>
+          {/* normal div with animation on it */}
+          {/* slide animation is there and its from left */}
+
+          {/* motion header set here */}
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          {/* motion div */}
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET's <br className="xl:block hidden" /> DO IT.
+              </h1>
+            </motion.div>
+
+            <motion.div {...headContentAnimation} className="flex flex-col gap-5">
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your unique and exclusive shirt with our brand-new
+                3Dcustomization tool. <strong>Unleash your imagination</strong>
+                {""} and define your own style.
+              </p>
+
+              <CustomButton
+
+              type="filled"
+              title="Customize It"
+              handleClick={()=>state.intro = false}
+              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              
+              /> {/* sending some props to this custom button */}
+
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
