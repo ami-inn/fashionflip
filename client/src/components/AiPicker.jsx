d18bc3a1985b0818dcba210d74ab2b4fcd7144d9
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-08-05 15:12:54 +0530

    almost completed without ai image creation

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import CustomButton from "./CustomButton";
 
-const AiPicker = () => {
+const AiPicker = ({ prompt, setPrompt, generatingImg ,handleSubmit}) => {
   return (
-    <div>
-      ai picker
-    </div>
-  )
-}
+    <div className="aipicker-container">
+      <textarea
+        placeholder="ask ai.."
+        className="aipicker-textarea"
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
 
-export default AiPicker
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="asking ai"
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="Ai Logo"
+              customStyles="text-xs"
+              handleClick={() => handleSubmit("logo")}
+            />
+             <CustomButton
+              type="filled"
+              title="Ai Full"
+              customStyles="text-xs"
+              handleClick={() => handleSubmit("full")}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AiPicker;
